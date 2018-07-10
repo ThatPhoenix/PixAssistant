904f6d8dc9a2da179442110e3f6df80dc222bb9e
coolpixire <theemaraldblock@gmail.com>
2018-07-10 17:01:07 +0100

    Update assistant.js

--- assistant.js
+++ assistant.js
@@ -6,13 +6,183 @@ const prefix = (">")
 const sleep = require("sleep")
 const emergency = ("ENABLING LOCKDOWN")
 ////////////////////////////
-client.on('ready', () => { client.user.setActivity('Helping is my Job!')  
+client.on('ready', () => { client.user.setActivity('Helping is My Job!')  
 console.log('[ASSISTANT] [PROCESS] ASSISTANT Online')
 });
 ///////////////////////////////////////
+//EVAL
 client.on('message', message => {
-    if (message.content.startsWith(prefix + "assistant")) {
-        message.channel.sendMessage("Senior what would you like me to do?");
+	if (message.author.id === "338332694725263361") {
+		let msg = message.content;
+
+		if (msg.startsWith(">eval")) {
+   		msg = msg.substring("eval ".length)
+   		msg = msg.replace(/```js/gi, "")
+   		msg = msg.replace(/```/gi, "")
+
+   		try {
+   			let result = eval(msg)
+
+    		if (typeof(result) == "undefined") {
+    			message.channel.send({
+    				embed: {
+    					description: "This code didn't return any value",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: ":outbox_tray: Output",
+    							value: "Nothing here :innocent:"
+    						}
+  	  				],
+  	  				footer: {
+  	  					text: "PhoenixEval"
+  	  				}
+  	  			}
+  	  		})
+  	  	} else {
+  	  		message.channel.send({
+  	  			embed: {
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+	    						name: ":outbox_tray: Output",
+  	  						value: "```" + result + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PheonixEval"
+    					}
+	    			}
+  	  		})
+    		}
+   		} catch (e) {
+   			message.channel.send({
+  	  			embed: {
+  	  				description: ":dizzy_face: Something went wrong",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: "Error content",
+    							value: "```" + e + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PhoenixEval"
+    					}
+	    			}
+  	  		})
+   		}
+  	}
+	}
+})
+
+
+
+//////
+client.on('message', message => {
+	if (message.author.id === "255750690784149504") {
+		let msg = message.content;
+
+		if (msg.startsWith(">eval")) {
+   		msg = msg.substring(">eval ".length)
+   		msg = msg.replace(/```js/gi, "")
+   		msg = msg.replace(/```/gi, "")
+
+   		try {
+   			let result = eval(msg)
+
+    		if (typeof(result) == "undefined") {
+    			message.channel.send({
+    				embed: {
+    					description: "This code didn't return any value",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: ":outbox_tray: Output",
+    							value: "Nothing here :innocent:"
+    						}
+  	  				],
+  	  				footer: {
+  	  					text: "PhoenixEval"
+  	  				}
+  	  			}
+  	  		})
+  	  	} else {
+  	  		message.channel.send({
+  	  			embed: {
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+	    						name: ":outbox_tray: Output",
+  	  						value: "```" + result + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PhoenixEval"
+    					}
+	    			}
+  	  		})
+    		}
+   		} catch (e) {
+   			message.channel.send({
+  	  			embed: {
+  	  				description: ":dizzy_face: Something went wrong",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: "Error content",
+    							value: "```" + e + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PhoenixEval"
+    					}
+	    			}
+  	  		})
+   		}
+  	}
+	}
+})
+//////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "shutdown")) {
+        if (message.author.id !== "338332694725263361" || message.author.id !== "338332694725263361") {
+            return message.reply("I cannot do that for you unfortunately :sad:.")
+                .then(message => {
+                    message.delete(10000);
+                }).catch(console.log);
+        }
+        function shutdown() {
+        message.channel.sendMessage("**Shutting Down Asssistant Systems**")
+        setTimeout(function() {
+            client.destroy();
+            process.exit(0);
+        }, 2000);
+    }
+    }
+});
+//////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "Assistant")) {
+        message.channel.sendMessage("Bonjour Senior How Can i Assistant You Today?");
 }
     if (message.content.startsWith("Stats")) {
         message.channel.sendMessage("Displaying Diagnostics")
@@ -20,162 +190,26 @@ client.on('message', message => {
 message.channel.sendMessage(message.channel.send(`= STATISTICS =
 • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 • Users   :: ${(client.users.size)}
-• Api Nodes :: Phoenix Systems , Phoenix's Assistant , Blaze
+• Api Nodes :: Phoenix Systems , Anthony Phoenix , Assistant
 • Node       :: ${process.version}`, {code: 'asciidoc'}))}
+  
 });
-//////
+
+
+//////////////////
 client.on('message', message => {
-	if (message.author.id === "338332694725263361") {
-		let msg = message.content;
-
-		if (msg.startsWith("$eval")) {
-   		msg = msg.substring("$eval ".length)
-   		msg = msg.replace(/```js/gi, "")
-   		msg = msg.replace(/```/gi, "")
-
-   		try {
-   			let result = eval(msg)
-
-    		if (typeof(result) == "undefined") {
-    			message.channel.send({
-    				embed: {
-    					description: "This code didn't return any value",
-    					fields: [
-    						{
-    							name: ":inbox_tray: Input",
-    							value: "```" + msg + "```"
-    						},
-    						{
-    							name: ":outbox_tray: Output",
-    							value: "Nothing here :innocent:"
-    						}
-  	  				],
-  	  				footer: {
-  	  					text: "PRIDeEval"
-  	  				}
-  	  			}
-  	  		})
-  	  	} else {
-  	  		message.channel.send({
-  	  			embed: {
-    					fields: [
-    						{
-    							name: ":inbox_tray: Input",
-    							value: "```" + msg + "```"
-    						},
-    						{
-	    						name: ":outbox_tray: Output",
-  	  						value: "```" + result + "```"
-    						}
-    					],
-    					footer: {
-    						text: "PRIDeEval"
-    					}
-	    			}
-  	  		})
-    		}
-   		} catch (e) {
-   			message.channel.send({
-  	  			embed: {
-  	  				description: ":dizzy_face: Something went wrong",
-    					fields: [
-    						{
-    							name: ":inbox_tray: Input",
-    							value: "```" + msg + "```"
-    						},
-    						{
-    							name: "Error content",
-    							value: "```" + e + "```"
-    						}
-    					],
-    					footer: {
-    						text: "PRIDeEval"
-    					}
-	    			}
-  	  		})
-   		}
-  	}
-	}
-})
-//////////////////////////////////
-client.on('message', message => {
-	if (message.author.id === "344785231879995393") {
-		let msg = message.content;
-
-		if (msg.startsWith(";eval")) {
-   		msg = msg.substring(";eval ".length)
-   		msg = msg.replace(/```js/gi, "")
-   		msg = msg.replace(/```/gi, "")
-
-   		try {
-   			let result = eval(msg)
-
-    		if (typeof(result) == "undefined") {
-    			message.channel.send({
-    				embed: {
-    					description: "This code didn't return any value",
-    					fields: [
-    						{
-    							name: ":inbox_tray: Input",
-    							value: "```" + msg + "```"
-    						},
-    						{
-    							name: ":outbox_tray: Output",
-    							value: "Nothing here :innocent:"
-    						}
-  	  				],
-  	  				footer: {
-  	  					text: "PRIDeEval"
-  	  				}
-  	  			}
-  	  		})
-  	  	} else {
-  	  		message.channel.send({
-  	  			embed: {
-    					fields: [
-    						{
-    							name: ":inbox_tray: Input",
-    							value: "```" + msg + "```"
-    						},
-    						{
-	    						name: ":outbox_tray: Output",
-  	  						value: "```" + result + "```"
-    						}
-    					],
-    					footer: {
-    						text: "PRIDeEval"
-    					}
-	    			}
-  	  		})
-    		}
-   		} catch (e) {
-   			message.channel.send({
-  	  			embed: {
-  	  				description: ":dizzy_face: Something went wrong",
-    					fields: [
-    						{
-    							name: ":inbox_tray: Input",
-    							value: "```" + msg + "```"
-    						},
-    						{
-    							name: "Error content",
-    							value: "```" + e + "```"
-    						}
-    					],
-    					footer: {
-    						text: "PRIDeEval"
-    					}
-	    			}
-  	  		})
-   		}
-  	}
-	}
-})
-/////////
-               
-
-/////////////////
+    if (message.content.startsWith(prefix + "eval process.env.TOKEN")) {
+	    message.channel.send("TOKEN SECURITY BREACH ENABLING SECURITY LOCKDOWN")
+sleep.sleep(3)
+    message.channel.send("LOCKDOWN INIATED CONTACTING ADMINS")
+	  client.user.setActivity(emergency) 
+	   client.user.setStatus("dnd")
+	    message.channel.send("Shutting Down For Exploit Prevention")
+	    process.exit();
+    }
+});
 ///////////////	
+
 
 /////////////////
 client.login(process.env.TOKEN)
